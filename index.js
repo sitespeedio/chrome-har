da8d1f0c06a20488de9b99e581a735eafcb9cb57
Jawad Stouli <jawad.stouli@gmail.com>
2017-06-29 20:41:02 +0800

    Added option to split multiple pages within the same frame

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const {
 const max = Math.max;
 
 const defaultOptions = {
-  includeResourcesFromDiskCache: false
+  includeResourcesFromDiskCache: false,
+  splitPagesWithinFrame: false
 };
 
 const isEmpty = o => !o;
@@ -190,8 +191,13 @@ module.exports = {
               entry => entry.__frameId === frameId
             );
 
-            if (rootFrameMappings.has(frameId) || previousFrameId) {
+            if (rootFrameMappings.has(frameId)) {
               // This is a sub frame, there's already a page for the root frame
+              continue;
+            }
+
+            if (options.splitPagesWithinFrame === false && previousFrameId) {
+              // We already have a page for the frame and the option says not to create a separate one
               continue;
             }
 
@@ -221,7 +227,7 @@ module.exports = {
             }
             const frameId =
               rootFrameMappings.get(params.frameId) || params.frameId;
-            const page = pages.find(page => page.__frameId === frameId);
+            const page = pages.filter(page => page.__frameId === frameId).pop();
             if (!page) {
               debug(
                 'Request will be sent with requestId ' +
@@ -364,7 +370,7 @@ module.exports = {
 
             const frameId =
               rootFrameMappings.get(params.frameId) || params.frameId;
-            const page = pages.find(page => page.__frameId === frameId);
+            const page = pages.filter(page => page.__frameId === frameId).pop();
             if (!page) {
               debug(
                 'Received network response for requestId ' +
