eab50a30a478d838ca00d560cd439482cbd2c8d9
soulgalore <peter@soulgalore.com>
2017-11-04 21:25:44 +0100

    push version to sitespeed.io docs

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Super simple release script for sitespeed.io
-# Lets use it it for now and make it better over time :)
-# You need np for this to work
 # npm install --global np
 np $1
+
+PACKAGE_VERSION=$(node -e 'console.log(require("./package").version)')
+
+echo $PACKAGE_VERSION  > ../sitespeed.io/docs/version/chrome-har.txt
