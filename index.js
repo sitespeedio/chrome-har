d803842c91db97789da938e0d80ebfbae014882f
Martino Trevisan <martino.trevisan@polito.it>
2018-05-22 13:28:57 +0200

    Fixed missing pushed objects in the HAR file (#20)
    * Fixed missing pushed objects in the HAR file
    
    Hi,
    I noticed that pushed H2 objects are often missing in the HAR file. I found the cause: lines 72-78 initialize the cache params for the pushed entry. As such, the object is considered as coming from the disk cache and not written in the HAR (line 654).
    Adding the simple check that I propose solves the issue.

--- index.js
+++ index.js
@@ -651,7 +651,8 @@ module.exports = {
 
     if (!options.includeResourcesFromDiskCache) {
       entries = entries.filter(
-        entry => entry.cache.beforeRequest === undefined
+        entry =>
+          entry.cache.beforeRequest === undefined || entry._was_pushed == 1
       );
     }
 
