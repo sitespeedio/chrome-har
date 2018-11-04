e9c9ad5ad293432ef183b17e7e7a4256d07c5e2a
soulgalore <peter@soulgalore.com>
2018-11-05 00:40:08 +0100

    Split the code to make it easier to read, part 1

--- index.js
+++ index.js
@@ -6,181 +6,24 @@ const urlParser = require('url');
 const uuid = require('uuid/v1');
 const moment = require('moment');
 const debug = require('debug')(name);
-
-const { parseRequestCookies, parseResponseCookies } = require('./lib/cookies');
+const ignoredEvents = require('./lib/ignoredEvents');
+const { parseRequestCookies } = require('./lib/cookies');
+const { getHeaderValue, parseHeaders } = require('./lib/headers');
 const {
-  calculateRequestHeaderSize,
-  calculateResponseHeaderSize,
-  getHeaderValue,
-  parseHeaders
-} = require('./lib/headers');
-
-const max = Math.max;
+  isHttp1x,
+  formatMillis,
+  parsePostData,
+  isSupportedProtocol,
+  toNameValuePairs
+} = require('./lib/util');
+const populateEntryFromResponse = require('./lib/entryFromResponse');
 
 const defaultOptions = {
   includeResourcesFromDiskCache: false
 };
-
 const isEmpty = o => !o;
 
-function formatMillis(time, fractionalDigits = 3) {
-  return Number(Number(time).toFixed(fractionalDigits));
-}
-
-function formatIP(ipAddress) {
-  if (typeof ipAddress !== 'string') {
-    return undefined;
-  }
-  // IPv6 addresses are listed as [2a00:1450:400f:80a::2003]
-  return ipAddress.replace(/^\[|]$/g, '');
-}
-
-function populateEntryFromResponse(entry, response, page) {
-  const responseHeaders = response.headers;
-  const cookieHeader = getHeaderValue(responseHeaders, 'Set-Cookie');
-
-  entry.response = {
-    httpVersion: response.protocol,
-    redirectURL: '',
-    status: response.status,
-    statusText: response.statusText,
-    content: {
-      mimeType: response.mimeType,
-      size: 0
-    },
-    headersSize: -1,
-    bodySize: -1,
-    cookies: parseResponseCookies(cookieHeader),
-    headers: parseHeaders(responseHeaders),
-    _transferSize: response.encodedDataLength
-  };
-
-  const locationHeaderValue = getHeaderValue(responseHeaders, 'Location');
-  if (locationHeaderValue) {
-    entry.response.redirectURL = locationHeaderValue;
-  }
-
-  entry.request.httpVersion = response.protocol;
-
-  if (response.fromDiskCache === true) {
-    if (isHttp1x(response.protocol)) {
-      // In http2 headers are compressed, so calculating size from headers text wouldn't be correct.
-      entry.response.headersSize = calculateResponseHeaderSize(response);
-    }
-
-    // h2 push might cause resource to be received before parser sees and requests it.
-    if (response.timing && !(response.timing.pushStart > 0)) {
-      entry.cache.beforeRequest = {
-        lastAccess: '',
-        eTag: '',
-        hitCount: 0
-      };
-    }
-  } else {
-    if (response.requestHeaders) {
-      entry.request.headers = parseHeaders(response.requestHeaders);
-
-      const cookieHeader = getHeaderValue(response.requestHeaders, 'Cookie');
-      entry.request.cookies = parseRequestCookies(cookieHeader);
-    }
-
-    if (isHttp1x(response.protocol)) {
-      if (response.headersText) {
-        entry.response.headersSize = response.headersText.length;
-      } else {
-        entry.response.headersSize = calculateResponseHeaderSize(response);
-      }
-
-      entry.response.bodySize =
-        response.encodedDataLength - entry.response.headersSize;
-
-      if (response.requestHeadersText) {
-        entry.request.headersSize = response.requestHeadersText.length;
-      } else {
-        // Since entry.request.httpVersion is now set, we can calculate header size.
-        entry.request.headersSize = calculateRequestHeaderSize(entry.request);
-      }
-    }
-  }
-
-  entry.connection = response.connectionId.toString();
-  entry.serverIPAddress = formatIP(response.remoteIPAddress);
-
-  function parseOptionalTime(timing, start, end) {
-    if (timing[start] >= 0) {
-      return formatMillis(timing[end] - timing[start]);
-    }
-    return -1;
-  }
-
-  const timing = response.timing;
-  if (timing) {
-    const blocked = formatMillis(
-      firstNonNegative([timing.dnsStart, timing.connectStart, timing.sendStart])
-    );
-
-    const dns = parseOptionalTime(timing, 'dnsStart', 'dnsEnd');
-    const connect = parseOptionalTime(timing, 'connectStart', 'connectEnd');
-    const send = formatMillis(timing.sendEnd - timing.sendStart);
-    const wait = formatMillis(timing.receiveHeadersEnd - timing.sendEnd);
-    const receive = 0;
-
-    const ssl = parseOptionalTime(timing, 'sslStart', 'sslEnd');
-
-    entry.timings = {
-      blocked,
-      dns,
-      connect,
-      send,
-      wait,
-      receive,
-      ssl
-    };
-
-    entry._requestTime = timing.requestTime;
-    entry.__receiveHeadersEnd = timing.receiveHeadersEnd;
-    if (timing.pushStart > 0) {
-      // use the same extended field as WebPageTest
-      entry._was_pushed = 1;
-    }
-
-    entry.time =
-      max(0, blocked) + max(0, dns) + max(0, connect) + send + wait + receive;
-
-    // Some cached responses generate a Network.requestServedFromCache event,
-    // but fromDiskCache is still set to false.
-    if (!entry.__servedFromCache) {
-      // wallTime is not necessarily monotonic, timestamp is. So calculate startedDateTime from timestamp diffs.
-      // (see https://cs.chromium.org/chromium/src/third_party/WebKit/Source/platform/network/ResourceLoadTiming.h?q=requestTime+package:%5Echromium$&dr=CSs&l=84)
-      const entrySecs =
-        page.__wallTime + (timing.requestTime - page.__timestamp);
-      entry.startedDateTime = moment.unix(entrySecs).toISOString();
-
-      const queuedMillis =
-        (timing.requestTime - entry.__requestWillBeSentTime) * 1000;
-      if (queuedMillis > 0) {
-        entry.timings._queued = formatMillis(queuedMillis);
-      }
-    }
-
-    if (entry.cache && entry.cache.beforeRequest) {
-      // lastAccess needs to be a valid date
-      entry.cache.beforeRequest.lastAccess = entry.startedDateTime;
-    }
-  } else {
-    entry.timings = {
-      blocked: -1,
-      dns: -1,
-      connect: -1,
-      send: 0,
-      wait: 0,
-      receive: 0,
-      ssl: -1,
-      comment: 'No timings available from Chrome'
-    };
-    entry.time = 0;
-  }
-}
+const max = Math.max;
 
 module.exports = {
   harFromMessages(messages, options) {
@@ -573,28 +416,6 @@ module.exports = {
           }
           break;
 
-        case 'Page.frameNavigated':
-        case 'Page.frameStoppedLoading':
-        case 'Page.frameClearedScheduledNavigation':
-        case 'Page.frameDetached':
-        case 'Page.frameResized':
-          // ignore
-          break;
-
-        case 'Page.lifecycleEvent':
-          // ignore for now, put in pageTimings later
-          break;
-
-        case 'Page.javascriptDialogOpening':
-        case 'Page.javascriptDialogClosed':
-        case 'Page.screencastFrame':
-        case 'Page.screencastVisibilityChanged':
-        case 'Page.colorPicked':
-        case 'Page.interstitialShown':
-        case 'Page.interstitialHidden':
-          // ignore
-          break;
-
         case 'Network.loadingFailed':
           {
             if (ignoredRequests.has(params.requestId)) {
@@ -627,19 +448,6 @@ module.exports = {
           }
           break;
 
-        case 'Network.webSocketCreated':
-        case 'Network.webSocketFrameSent':
-        case 'Network.webSocketFrameError':
-        case 'Network.webSocketFrameReceived':
-        case 'Network.webSocketClosed':
-        case 'Network.webSocketHandshakeResponseReceived':
-        case 'Network.webSocketWillSendHandshakeRequest':
-          // ignore, sadly HAR file format doesn't include web sockets
-          break;
-
-        case 'Network.eventSourceMessageReceived':
-          // ignore
-          break;
         case 'Network.resourceChangedPriority':
           {
             const entry = entries.find(
@@ -660,7 +468,8 @@ module.exports = {
           break;
 
         default:
-          debug(`Unhandled event: ${method}`);
+          // Keep the old functionallity and log unknown events
+          ignoredEvents(method);
           break;
       }
     }
@@ -729,68 +538,3 @@ module.exports = {
     };
   }
 };
-
-function toNameValuePairs(object) {
-  return Object.keys(object).reduce((result, name) => {
-    const value = object[name];
-    if (Array.isArray(value)) {
-      return result.concat(
-        value.map(v => {
-          return { name, value: v };
-        })
-      );
-    } else {
-      return result.concat([{ name, value }]);
-    }
-  }, []);
-}
-
-function parseUrlEncoded(data) {
-  const params = urlParser.parse(`?${data}`, true).query;
-  return toNameValuePairs(params);
-}
-
-function parsePostData(contentType, postData) {
-  if (isEmpty(contentType) || isEmpty(postData)) {
-    return undefined;
-  }
-
-  try {
-    if (/^application\/x-www-form-urlencoded/.test(contentType)) {
-      return {
-        mimeType: contentType,
-        params: parseUrlEncoded(postData)
-      };
-    }
-    if (/^application\/json/.test(contentType)) {
-      return {
-        mimeType: contentType,
-        params: toNameValuePairs(JSON.parse(postData))
-      };
-    }
-    // FIXME parse multipart/form-data as well.
-  } catch (e) {
-    debug(`Unable to parse post data '${postData}' of type ${contentType}`);
-    // Fall back to include postData as text.
-  }
-
-  return {
-    mimeType: contentType,
-    text: postData
-  };
-}
-
-function isSupportedProtocol(url) {
-  return /^https?:/.test(url);
-}
-
-function isHttp1x(version) {
-  return version.toLowerCase().startsWith('http/1.');
-}
-
-function firstNonNegative(values) {
-  for (let i = 0; i < values.length; ++i) {
-    if (values[i] >= 0) return values[i];
-  }
-  return -1;
-}
