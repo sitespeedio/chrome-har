f91732a679f2bc3d342e24ea3df3c241abcccf20
Peter Hedenskog <peter@soulgalore.com>
2019-09-14 14:59:00 +0200

    Handle when we have request without a response/timings. (#53)
    Its better too swallow the error and produce a working HAR instead
    of throw the error. If we got time we can look more into this.

--- index.js
+++ index.js
@@ -369,7 +369,7 @@ module.exports = {
               continue;
             }
 
-            const timings = entry.timings;
+            const timings = entry.timings || {};
             timings.receive = formatMillis(
               (params.timestamp - entry._requestTime) * 1000 -
                 entry.__receiveHeadersEnd
@@ -378,9 +378,9 @@ module.exports = {
               max(0, timings.blocked) +
               max(0, timings.dns) +
               max(0, timings.connect) +
-              timings.send +
-              timings.wait +
-              timings.receive;
+              max(0, timings.send) +
+              max(0, timings.wait) +
+              max(0, timings.receive);
 
             // For cached entries, Network.loadingFinished can have an earlier
             // timestamp than Network.dataReceived
@@ -388,20 +388,24 @@ module.exports = {
             // encodedDataLength will be -1 sometimes
             if (params.encodedDataLength >= 0) {
               const response = entry.response;
+              if (response) {
+                response._transferSize = params.encodedDataLength;
+                response.bodySize = params.encodedDataLength;
 
-              response._transferSize = params.encodedDataLength;
-              response.bodySize = params.encodedDataLength;
+                if (
+                  isHttp1x(response.httpVersion) &&
+                  response.headersSize > -1
+                ) {
+                  response.bodySize -= response.headersSize;
+                }
 
-              if (isHttp1x(response.httpVersion) && response.headersSize > -1) {
-                response.bodySize -= response.headersSize;
-              }
-
-              const compression = Math.max(
-                0,
-                response.content.size - response.bodySize
-              );
-              if (compression > 0) {
-                response.content.compression = compression;
+                const compression = Math.max(
+                  0,
+                  response.content.size - response.bodySize
+                );
+                if (compression > 0) {
+                  response.content.compression = compression;
+                }
               }
             }
           }
